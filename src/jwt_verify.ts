97abf1d29f89f8d064958a49bb1b4cac09230b34
Kai <orozobekov.kai@gmail.com>
2023-03-19 06:46:04 -0400

    feat: express middleware modified - bodyparser, cors, fn for verifying if request contains valid token

---
+++ src/jwt_verify.ts
@@ -0,0 +1,19 @@
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+import { pino } from "pino";
+
+const logger = pino();
+
+export const verifyUser = async (token: string) => {
+  const verifier = CognitoJwtVerifier.create({
+    userPoolId: process.env["USER_POOL_ID"] as string,
+    tokenUse: "id",
+    clientId: process.env["USER_POOL_CLIENT_ID"] as string,
+  });
+  try {
+    const payload = await verifier.verify(token);
+    return { payload: payload, verified: true };
+  } catch (e) {
+    logger.info("Token validation failed!");
+    return { error: e, verified: false };
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,72 @@
 import express, { Express, Response, Request } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import bodyParser from "body-parser";
 import * as dotenv from "dotenv";
+
+import { verifyUser } from "./jwt_verify.js";
+import startPostgres from "./db/index.js";
+import router from "./routes/index.js";
 
 const app: Express = express();
 
-/* const corsOptions = {
+const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
-}; */
+};
 
 dotenv.config();
+startPostgres();
 
 // Use CORS middleware so we can make requests across origins
-app.use(cors());
+app.use(cors(corsOptions));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(async (req: Request, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader !== undefined) {
+    const response = await verifyUser(authHeader.split(" ")[1]);
+    if (response.verified) {
+      const payload = response.payload;
+      if (payload !== undefined) {
+        res.locals.user = payload.email;
+        res.locals.role = payload["custom:role"];
+      }
+      next();
+    } else {
+      res.status(401).json({
+        status: "error",
+        error: {
+          message: "Not Authorized",
+          code: 401,
+        },
+      });
+    }
+  } else {
+    res.status(401).json({
+      status: "error",
+      error: {
+        message: "Not Authorized",
+        code: 401,
+      },
+    });
+  }
+});
 
 // Use security middleware
 app.use(helmet());
+
+app.use("/v1/api", router);
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((_req: Request, res: Response) => {
   res.status(404).json({
     status: "error",
     error: {
-      message: "not found",
+      message: "Not Found",
       code: 404,
     },
   });
