70ab7e6d266105b544d18200987af58dd36c4a6a
Kai <orozobekov.kai@gmail.com>
2023-03-19 15:03:58 -0400

    fix: req.body.role replaced res.locals.role

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,13 @@
 import express, { Response, Request, NextFunction } from "express";
 import postItem from "./api/post_item.js";
 import deleteItem from "./api/delete_item.js";
+// import getAllItems from "./api/get_items.js";
 
 // Create a router that we can use to mount our API
 const router = express.Router();
 
 const ifSeller = (req: Request, res: Response, next: NextFunction) => {
-  const role = req.body.role;
+  const role = res.locals.role;
   return role === "seller"
     ? next()
     : res.status(401).json({
@@ -21,5 +22,6 @@ const ifSeller = (req: Request, res: Response, next: NextFunction) => {
 
 router.post("/items", ifSeller, postItem);
 router.delete("/items", ifSeller, deleteItem);
+//router.get("/items", getAllItems);
 
 export default router;
