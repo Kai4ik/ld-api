67acaa998418442bcaf40f7c455c0a4f1f476ba2
Kai <orozobekov.kai@gmail.com>
2023-03-19 06:43:14 -0400

    refactor: tsconfig allow exprerimentalDecorators, new env variables introduced for aws cognito

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PORT: string
+      PORT: string;
+      USER_POOL_ID: string;
+      USER_POOL_CLIENT_ID: string;
     }
   }
 }
 
-export {}
+export {};
